a07cb1e42c658fed5f2dd646d0292a3b8154a1d8
wylgithub <ltwuyanlong@sina.com>
2015-08-13 05:13:54 +0800

    java推送github

---
+++ src/com/JavaLearnByMyself/TestGithub.java
@@ -0,0 +1,11 @@
+package com.JavaLearnByMyself;
+
+public class TestGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello github");
+	}
+
+}
